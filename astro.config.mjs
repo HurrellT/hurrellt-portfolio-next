9657b810b53eaa635b7da910dba504ec9bd15394
Tomas Hurrell <thurrell@portfoliopersonal.com>
2024-09-29 22:31:50 -0300

    feat: agrego shadcn ui + theme management

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), icon(), react()]
 });
