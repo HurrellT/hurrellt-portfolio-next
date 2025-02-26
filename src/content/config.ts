688a4d05002a25a01d53ae2cd3c929434326afc6
Tomas Hurrell <hurrelltomas@gmail.com>
2025-02-26 15:51:37 -0300

    feat: added personal projects to the portfolio

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,10 +12,22 @@ const projects = defineCollection({
   }),
 });
 
+const personalProjects = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    img: z.string(),
+    description: z.string(),
+    link: z.string().url(),
+    sourcecode: z.string().url(),
+    linkAvailable: z.boolean(),
+    sourcecodeAvailable: z.boolean(),
+  }),
+});
+
 const jobs = defineCollection({
   schema: z.object({
     name: z.string(),
   }),
 });
 
-export const collections = { projects, jobs };
+export const collections = { projects, jobs, personalProjects };
---
+++ src/content/personalProjects/1-chanchito.md
@@ -0,0 +1,10 @@
+---
+title: Chanchito
+img: chanchito.png
+description: Expense tracking app - Work in progress
+link: https://github.com/hurrellt
+sourcecode: 'https://github.com/hurrellt'
+linkAvailable: false
+sourcecodeAvailable: false
+---
+Expense tracking app with a focus on simplicity and credit card management. The app is built using Expo, React Native and TypeScript. The app is currently in development and is not yet available for download.
